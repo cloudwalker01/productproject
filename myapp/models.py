49e62fba4e97e9a739c7efef932d411a8ec2970b
Arnav Agarwal <89313381+cloudwalker01@users.noreply.github.com>
2023-05-06 12:13:57 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -9,3 +9,34 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+    
+
+# class Author(models.Model):
+#     fname=models.CharField(max_length=50)
+#     lname=models.CharField(max_length=50)
+#     email=models.EmailField()
+
+#     def __str__(self):
+#         return self.fname
+
+# class Publisher(models.Model):
+#     name=models.CharField(max_length=50)
+#     street=models.CharField(max_length=50)
+#     city=models.CharField(max_length=50)
+#     state=models.CharField(max_length=50)
+#     country=models.CharField(max_length=50)
+#     website=models.URLField(max_length=50)
+    
+#     def __str__(self):
+#         return self.name
+
+# class Book(models.Model):
+#     title=models.CharField(max_length=50)
+#     date=models.DateField(max_length=50)
+#     author=models.ManyToManyField(Author)
+#     publisher=models.ForeignKey(Publisher, on_delete=models.CASCADE)
+
+#     def __str__(self):
+#         return self.title
+
