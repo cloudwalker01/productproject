64f595d4a851f07755de1d7b25fe325b8cc5680d
Arnav Agarwal <89313381+cloudwalker01@users.noreply.github.com>
2023-05-06 11:35:47 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -1,12 +1,11 @@
 from django.db import models
-from django import forms
 
 # Create your models here.
 
 class Product(models.Model):
     title=models.CharField(max_length=50)
     price=models.PositiveIntegerField()
-    description=models.CharField(widget=forms.CheckboxInput)
+    description=models.CharField()
 
     def __str__(self):
         return self.title
